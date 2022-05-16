695b8fbc5eeab3c98f46afdc69d0a187ea3c173d
Sinus <sinus@sinpi.net>
2022-05-17 01:21:29 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderDupes
+{
+	class Program
+	{
+		static string[] Folders = { @"C:\FOTO\LRplugins" };
+
+		static List<FileInfo> Files;
+		static void Main(string[] args)
+		{
+			Files = new List<FileInfo>();
+			foreach (string f in Folders) {
+				ReadFiles(f);
+			}
+		}
+
+		static void ReadFiles(string path)
+		{
+			DirectoryInfo di = new DirectoryInfo(path);
+
+			FileInfo[] files = di.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+			Files.AddRange(files);
+
+			DirectoryInfo[] directories = di.GetDirectories("*.*", SearchOption.TopDirectoryOnly);
+		}
+
+		static void RunComparison(CompareMode mode = CompareMode.Name | CompareMode.Size)
+		{
+
+		}
+
+		[Flags]
+		public enum CompareMode {
+			Name = 0b0001,
+			Date = 0b0010,
+			Size = 0b0100,
+			Hash = 0b1000
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("FolderDupes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FolderDupes")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("4c204845-a7b8-4a00-9a18-aee7fe5915e9")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
